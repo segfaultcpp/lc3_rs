ad2973fea17403fc3537c2ff3a2a18618aa15312
segfaultcpp <some@email.com>
2023-05-10 15:42:59 +0500

    linux support

---
+++ build_cpp.sh
@@ -0,0 +1,2 @@
+sudo gcc -c -Wall -o cpp/terminal_setup.o cpp/terminal_setup.cpp
+sudo ar r cpp/terminal_setup.a cpp/terminal_setup.o
--- cpp/terminal_setup.cpp
+++ cpp/terminal_setup.cpp
@@ -30,6 +30,39 @@
         }
     }
 #elif __linux__
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <sys/time.h>
+    #include <sys/types.h>
+    #include <sys/termios.h>
+    #include <sys/mman.h>
+
+    struct termios original_tio;
+
+    void disable_input_buffering() {
+        tcgetattr(STDIN_FILENO, &original_tio);
+        struct termios new_tio = original_tio;
+        new_tio.c_lflag &= ~ICANON & ~ECHO;
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+    }
+
+    void restore_input_buffering() {
+        tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+    }
+
+    extern "C" {
+        uint16_t check_key() {
+            fd_set readfds;
+            FD_ZERO(&readfds);
+            FD_SET(STDIN_FILENO, &readfds);
+
+            struct timeval timeout;
+            timeout.tv_sec = 0;
+            timeout.tv_usec = 0;
+            return select(1, &readfds, NULL, NULL, &timeout) != 0;
+        }
+    }        
 #endif
 
 void handle_interrupt(int signal) {
